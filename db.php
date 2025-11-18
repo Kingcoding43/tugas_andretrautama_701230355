51e8bcd28e2ac739eb24bfa6631679ecbb8a0418
Andre Trautama <andretaurapratama@gmail.com>
2025-11-19 00:53:30 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$host = "localhost";
+$host = "127.0.0.1";
 $user = "root";
 $pass = "";
 $db   = "login_app";
@@ -9,4 +9,5 @@ $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
+
 ?>
